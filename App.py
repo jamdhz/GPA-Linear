a486e40042e20b178695a07a213f645dc14f6617
jamdhz <contactmejdh@gmail.com>
2025-05-17 11:45:06 +0700

    App.py updated, model loaded

--- App.py
+++ App.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+import joblib
 
 st.title("GPA Prediction Model")
 
@@ -14,10 +15,33 @@ branch = st.selectbox("Branch", ["P", "A"])
 level = st.slider("Level", 1, 6, 1)
 age = st.slider("Age", 18, 25, 18)
 
+def preprocess_input(spec, gender, branch):
+    spec_map = {"CS": 0, "MIS": 1, "ACC": 2, "BA": 3}
+    gender_map = {"F": 0, "M": 1}
+    branch_map = {"P": 0, "A": 1}
+
+    return spec_map[spec], gender_map[gender], branch_map[branch]
+
+
 def predict_gpa(spec, gender, st_hr, school_av, branch, level, age):
-    # Nanti pake model
-    print("A")
+
+    spec, gender, branch = preprocess_input(spec, gender, branch)
+
+    with open('gpa_model_1.pkl', 'rb') as file:
+        model = joblib.load(file)
+
+    input_data = np.array([[spec, gender, st_hr, school_av, branch, level, age]])
+
+    gpa_prediction = model.predict(input_data)
+
+    return gpa_prediction[0]
+
+
+def convert_to_4_scale(gpa):
+    return (gpa / 100) * 4
 
 if st.button("Predict GPA"):
     gpa = predict_gpa(spec, gender, st_hr, school_av, branch, level, age)
     st.success(f"Predicted GPA: {gpa}")
+    gpa_4_scale = convert_to_4_scale(gpa)
+    st.success(f"Predicted GPA: {gpa:.2f} (Raw), {gpa_4_scale:.2f} (0-4 Scale)")
